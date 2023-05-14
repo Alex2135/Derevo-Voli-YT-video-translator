1b9d1733421ee2e510401ce058e851a865b93251
Alex2135 <leha3521@gmail.com>
2023-05-14 14:04:16 +0300

    Basic structure of the app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Derevo-Voli-YT-video-translator
+
+Create the environment
+```bash
+python3 -m venv venv
+```
+
+Activate environment
+```bash
+source venv/bin/activate
+```
+or for `Windows` `Visual Code`
+```bash
+.\venv\Scripts\activate
+```
+
+Install dependencies
+```bash
+pip3 install -r requirements.txt
+```
---
+++ notebook_requirements.sh
@@ -0,0 +1,14 @@
+!pip3 install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
+!pip3 install git+https://github.com/openai/whisper.git
+!pip3 install deep-translator tqdm
+!pip3 install TTS==0.13.3
+!pip3 install git+https://github.com/egorsmkv/ukrainian-accentor.git
+!pip install ukrainian-word-stress
+!pip install git+https://github.com/huggingface/transformers
+!pip install transformers[sentencepiece]
+!pip3 install sentencepiece
+!pip uninstall protobuf -y
+!pip install protobuf==4.21
+
+import os
+os.kill(os.getpid(), 9)
