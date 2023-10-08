3abad399d0a11a53b053ead5d3915f1e0c1424e9
Alex2135 <leha3521@gmail.com>
2023-10-08 15:25:27 +0300

    Updated text-to-speech model

--- text_to_speech/text_to_speech_models.py
+++ text_to_speech/text_to_speech_models.py
@@ -1,6 +1,7 @@
 import os
 from abc import ABC
 
+import torch
 from ukrainian_tts.tts import TTS, Voices, Stress
 
 
@@ -12,6 +13,7 @@ class TtsModel(ABC):
     
 
 class UkrainianTtsEspnet:
+    
     tts_voices: dict[str, any] = {
         "Tetiana": Voices.Tetiana.value,
         "Mykyta": Voices.Mykyta.value,
@@ -20,12 +22,15 @@ class UkrainianTtsEspnet:
         "Oleksa": Voices.Oleksa.value,
     }
     
+    def __init__(self) -> None:
+        device = "gpu" if torch.cuda.is_available() else "cpu"
+        self.tts = TTS(device=self.device)  # can try gpu, mps
+    
     def run_tts(self, ukrainian_stressified_text: str, voice: str) -> str:
-        tts = TTS(device="cpu")  # can try gpu, mps
         if not os.path.exists("/tmp/audio"):
             os.mkdir("/tmp/audio")
         with open("/tmp/audio/test.wav", mode="wb") as file:
-            _, output_text = tts.tts(
+            _, output_text = self.tts.tts(
                 ukrainian_stressified_text, 
                 self.tts_voices[voice], 
                 Stress.Dictionary.value, 
